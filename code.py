859def0c7e7f65fd75020f5ccd036284bd87bdc9
Francis <montesfrancis888@gmail.com>
2026-08-04 15:02:18 +0800

    optimized code, added choice system and user selection (easter egg too)

--- code.py
+++ code.py
@@ -1,12 +1,40 @@
 import time
+
+user = input("Enter your name: ")
+
+if user == "admin":
+    print("Welcome, Developer! Thank you for your service!")
+else:
+    print (f"Hello, {user}! Welcome to the storage unit!")
+
+
+def delayed_print(text, delay=0.5):
+    for char in text:
+        print (char, end="", flush=True)
+        time.sleep(delay)
+    print()
+
 data = {
     "fruits":["apple","banana","orange","grape"],
     "games":["CODMW","Roblox","TF2"],
     "schools":["UP","DLSU","OLFU","SNSM","MSHS"],
 }
-for school in data["games"]:
-    for char in school:
-        print(char, end="", flush=True)
-        time.sleep(0.4)
-    print()
+shelf_choice = input("Which shelf? (fruits, games, schools): ")
+if shelf_choice == "schools":
+    for school in data["games"]:
+      delayed_print(school, 0.05) 
 
+elif shelf_choice == "fruits":
+    for fruit in data["fruits"]:
+        delayed_print(fruit, 0.05)
+
+elif shelf_choice == "games":
+    for games in data["games"]:
+        delayed_print(games, 0.05)
+
+elif shelf_choice == "secret060108":
+    delayed_print("Access granted! Welcome to the Developers birthday room!", 0.05)
+
+else:
+    delayed_print("Invalid shelf choice. Please try again.", 0.05)
+    
